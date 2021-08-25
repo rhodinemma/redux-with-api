3a31a74ca62f1b699c55b029d13035a744652a56
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2021-08-25 18:12:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Users from './components/users';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Redux Tutorial</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/reducers/userReducer.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_USERS } from "../types";
+
+const initialState = {
+  users: [],
+  loading: true,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
